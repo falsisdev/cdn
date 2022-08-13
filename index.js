5ce302f66fb694adb928be40f219f95beb61fb06
Falsis <keremtokul@hotmail.com>
2022-08-13 11:33:29 +0300

    edit: edited somethings

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = new express();
 const path = require("path");
 const anime = require(path.join(process.cwd(), "/anime/index.js"))
-app.listen(3000, () => {
+const animeParams = ["images", "info"]
+const params = ["anime"]
+app.listen(3000, (req, res) => {
     console.log("Tüm cdn Proje Başlatıldı.")
 })
 app.get("/", function(req, res) {
@@ -12,6 +14,12 @@ app.get("/", function(req, res) {
     })
 })
 app.get('/anime/:type', (req, res) => {
+    if(animeParams.includes(req.params.type) == false) {
+        res.json({
+            code: 404,
+            response: "Page not Found"
+        })
+    }
     switch(req.params.type) {
     case "images":
     anime.images(req, res)
